d15daffc8da27c009ce526f06e791ade371ae62b
Raimon <60296186+Neune0@users.noreply.github.com>
2025-03-10 14:53:52 +0100

    first commit

---
+++ src/main/java/com/example/progetton/ProgettonApplication.java
@@ -0,0 +1,13 @@
+package com.example.progetton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/progetton/ProgettonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.progetton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
