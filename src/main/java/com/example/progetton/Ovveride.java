2bf2fda93e5758602da9393f59cf18dd185f79f8
pepperagosta7 <giusepperagosta29@gmail.com>
2025-03-10 15:14:18 +0100

    First files

---
+++ src/main/java/com/example/progetton/Ovveride.java
@@ -0,0 +1,5 @@
+package com.example.progetton;
+
+public @interface Ovveride {
+
+}
---
+++ src/main/java/com/example/progetton/ShippingService.java
@@ -0,0 +1,7 @@
+package com.example.progetton;
+
+public interface ShippingService {
+
+    double calculateShipping(String country, double weight);
+
+}
